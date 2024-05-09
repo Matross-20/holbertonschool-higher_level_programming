389c51a2ae98d4b9bb1f442fda04f61be8b91867
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-05-09 17:09:53 +0200

    Copy - Cut - Paste

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
 
-print(f"First 3 letters: [Hol]")
-print(f"Last 2 letters: [on]")
-print(f"Middle word: [bert]")
+print(f"First 3 letters: Hol")
+print(f"Last 2 letters: on")
+print(f"Middle word: olberto")
