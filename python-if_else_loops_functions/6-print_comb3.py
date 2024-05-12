94ca4d6a33862009f510bd27b1413fa65277fc42
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-05-12 13:29:54 +0200

    nventing is a combination of brains and materials. The more brains you use, the less material you need

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-
-for i in range(10):
-    for j in range(i+1, 10):
-        print("{:02d}".format(i) + ", " + "{:02d}".format(j), end=", " if i < 9 and j < 9 else "\n")
-
+for x in range(0, 10):
+    for y in range(x + 1, 10):
+        if x == y:
+            continue
+        if x == 8 and y == 9:
+            print("{}""{}" .format(x, y))
+            continue
+        print("{}""{}" .format(x, y), end=', ')
