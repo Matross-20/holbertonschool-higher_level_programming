1a29e84f89430361936e44914beab1b9252cf9e8
Matross-20 <matrossmthawelanga@gmail.com>
2024-05-12 13:30:27 +0000

    Who are you?

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Print all names defined by hidden_4 module."""
     import hidden_4
-    for names in dir(hidden_4):
-        if names[0] != '_' and names[1] != '_':
-            print(names)
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
