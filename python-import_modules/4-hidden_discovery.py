642dcc86c8b172585810f97bea8b11b041a26de8
Matross-20 <matrossmthawelanga@gmail.com>
2024-05-12 14:16:42 +0000

    Who are you?

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 
+import importlib.util
+
+def print_module_names():
+    # Load the compiled module hidden_4.pyc
+    spec = importlib.util.spec_from_file_location("hidden_4", "/tmp/hidden_4.pyc")
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    # Get all names defined in the module
+    module_names = dir(module)
+
+    # Print names that do not start with '__' in alphabetical order
+    for name in sorted(module_names):
+        if not name.startswith('__'):
+            print(name)
+
 if __name__ == "__main__":
-    from hidden_4 import *
-    allf = dir()
-    for i in range(0, len(allf)):
-        if allf[i] [:2] != "__":
-            print("{:s}".format(allf[i]))
+    print_module_names()
+
