eeaf0e99ec5cff1b813aff23676ca0f76fc4a59d
Matross-20 <matrossmthawelanga@gmail.com>
2024-05-17 10:01:09 +0000

    Tuples addition

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -17,8 +17,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = (0, 0)
     else:
         tuple_b = (tuple_b[0], tuple_b[1])
-    
     result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    
     return result
-
