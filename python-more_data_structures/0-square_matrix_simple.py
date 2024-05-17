bd2ed47d0ddc6976854411112b715a383e107e49
Matross-20 <matrossmthawelanga@gmail.com>
2024-05-17 14:42:13 +0000

    Squared simple

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+
+    new_matrix = []
+
+    for row in matrix:
+
+        new_row = []
+
+        for element in row:
+
+            new_row.append(element ** 2)
+
+        new_matrix.append(new_row)
+
+    return new_matrix
