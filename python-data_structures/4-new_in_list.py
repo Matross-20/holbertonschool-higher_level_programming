8e450d05801aea109a2fcfbd794643c8e9278d02
Matross-20 <matrossmthawelanga@gmail.com>
2024-05-17 09:21:33 +0000

    Replace in a copy

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,12 +4,9 @@ def new_in_list(my_list, idx, element):
     # Return a copy of the original list if idx is out of range or negative
     if idx < 0 or idx >= len(my_list):
         return my_list[:]
-    
     # Create a copy of the list
     new_list = my_list[:]
-    
     # Replace the element at the specified index
-    new_list[idx] = element
-    
+    new_list[idx] = element  
     # Return the new list
     return new_list
