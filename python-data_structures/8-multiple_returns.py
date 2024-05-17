2354d1e045f923b26d89802645b18c80c3c9046e
Matross-20 <matrossmthawelanga@gmail.com>
2024-05-17 10:32:41 +0000

    Tuples addition

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    # Check if the sentence is empty
+
     if sentence == "":
         return (0, None)
-    
-    # Return the length of the sentence and the first character
-    return (len(sentence), sentence[0])
 
-print(multiple_returns("Hello"))
-print(multiple_returns(""))
-print(multiple_returns("Python"))  
-print(multiple_returns("A"))
+    return (len(sentence), sentence[0])
