f2b7d16f0d581cbab71ba3722ffec77638466775
Matross-20 <matrossmthawelanga@gmail.com>
2024-05-18 14:48:00 +0000

    Best score

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+
+    if not a_dictionary:
+        return None
+
+    max_key = None
+    max_value = float('-inf')
+
+    for key, value in a_dictionary.items():
+        if value > max_value:
+            max_value = value
+            max_key = key
+
+    return max_key
