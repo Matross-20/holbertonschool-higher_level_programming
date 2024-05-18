bf1da6f6edabd9bdb3c8d20ed3527488fb8d7298
Matross-20 <matrossmthawelanga@gmail.com>
2024-05-18 16:16:06 +0000

    Safe list printing

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        while count < x:
+            print(my_list[count], end="")
+            count += 1
+    except IndexError:
+        pass
+    finally:
+        print()  # Ensure new line at the end
+    return count
