5fe2b0fdb33f90e0b31ee429b94d31fd14b855a0
Matross-20 <matrossmthawelanga@gmail.com>
2024-05-18 13:02:07 +0000

    Search and replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-
 def search_replace(my_list, search, replace):
-    # Create a new list using a list comprehension
-    new_list = [replace if element == search else element for element in my_list]
+    new_list = []
+    for element in my_list:
+        if element == search:
+            new_list.append(replace)
+        else:
+            new_list.append(element)
     return new_list
