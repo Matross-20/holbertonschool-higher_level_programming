b580b85b3d79e82962212af1e6ac675e6c9212c6
Matross-20 <matrossmthawelanga@gmail.com>
2024-05-25 16:05:24 +0000

    Max integer - Unittest

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module to find the max integer in a list
+"""
+
+
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
+    """
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
+
+guillaume@ubuntu:~/$ 
+guillaume@ubuntu:~/$ cat 6-main.py
+#!/usr/bin/python3
+max_integer = __import__('6-max_integer').max_integer
+
+print(max_integer([1, 2, 3, 4]))
+print(max_integer([1, 3, 4, 2]))
