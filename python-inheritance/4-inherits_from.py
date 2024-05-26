80e978e17e8c8c3f15ea3554836b09905af4b606
Matross-20 <matrossmthawelanga@gmail.com>
2024-05-26 15:12:09 +0000

    Only sub class of

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 
-def inherits_from(obj, a_class):
-    if isinstance(obj, a_class) and \
-       issubclass(a_class, obj.__class__) is False:
-        return True
+"""Defines a class-checking function."""
 
-    return False
+
+def inherits_from(obj, a_class):
+    """Check if an object is exactly an instance of a given class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
+    """
+    return type(obj) is not a_class and issubclass(type(obj), a_class)
