b94277ccdfdda5896b61ac9992d263b89db4e059
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-05-27 16:41:21 +0200

    Shapes, Interfaces, and Duck Typing

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,14 +1,12 @@
-#!/usr/bin/python3
-
 from abc import ABC, abstractmethod
 import math
 
 class Shape(ABC):
-
+    
     @abstractmethod
     def area(self):
         pass
-
+    
     @abstractmethod
     def perimeter(self):
         pass
@@ -16,10 +14,10 @@ class Shape(ABC):
 class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
-
+    
     def area(self):
         return math.pi * (self.radius ** 2)
-
+    
     def perimeter(self):
         return 2 * math.pi * self.radius
 
@@ -27,19 +25,13 @@ class Rectangle(Shape):
     def __init__(self, width, height):
         self.width = width
         self.height = height
-
+    
     def area(self):
         return self.width * self.height
-
+    
     def perimeter(self):
         return 2 * (self.width + self.height)
 
 def shape_info(shape):
     print(f"Area: {shape.area()}")
     print(f"Perimeter: {shape.perimeter()}")
-
-circle = Circle(5)
-shape_info(circle)
-
-rectangle = Rectangle(4, 7)
-shape_info(rectangle)
