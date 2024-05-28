5d4cf93c1115873dc9ef85c516871399a1e1b4b5
Matross-20 <matrossmthawelanga@gmail.com>
2024-05-28 14:13:25 +0000

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Module to read a file from argument"""
 
+
 def read_file(filename=""):
     """
     Reads a UTF-8 encoded text file and prints its content to stdout.
 
     Args:
-        filename (str): The path to the file to be read. Default is an empty string.
+        filename (str): The path to the file to be read.
     """
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end="")
