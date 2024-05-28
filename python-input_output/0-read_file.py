d4759de13068f036a700f616670d60704a7707de
Matross-20 <matrossmthawelanga@gmail.com>
2024-05-28 14:01:13 +0000

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
- 
+
 def read_file(filename=""):
+    """
+    Reads a UTF-8 encoded text file and prints its content to stdout.
+
+    Args:
+        filename (str): The path to the file to be read. Default is an empty string.
+    """
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read())
