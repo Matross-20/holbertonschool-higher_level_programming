bc8c43026115c43f0ed0f11ef0ff678334201421
Matross-20 <matrossmthawelanga@gmail.com>
2024-06-06 09:07:14 +0000

    Develop a Simple API using Python with Flask

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""A module to handle simple flask requests"""
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+users = {}
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+
+@app.route("/data")
+def get_data():
+    return jsonify(list(users.keys()))
+
+
+@app.route("/status")
+def status():
+    return "OK"
+
+
+@app.route("/users/<username>")
+def get_user(username):
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    user_data = request.get_json()
+    username = user_data.get("username")
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+    users[username] = user_data
+    return jsonify({"message": "User added", "user": user_data}), 201
+
+
+if __name__ == "__main__":
+    app.run()
