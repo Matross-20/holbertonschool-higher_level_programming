fa9274be77808e6498037bba1f04b33e19025fe4
Matross-20 <matrossmthawelanga@gmail.com>
2024-06-23 16:39:22 +0000

    Full description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,3 @@
--- SQL command to list all rows of the table in the database
+-- Script that prints the full description of the table first_table from the database hbtn_0c_0 in MySQL server
 
-SELECT *
-FROM first_table;
+SHOW CREATE table first_table;
