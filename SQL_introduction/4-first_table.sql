1a12d68ac092b94540a28718ad9a018fdfa97490
Matross-20 <matrossmthawelanga@gmail.com>
2024-06-25 18:06:49 +0000

    Cheating is bad

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -3,3 +3,19 @@ CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
 );
+
+-- Inserting the data
+INSERT INTO first_table (id, name, score) VALUES
+(1, 'A', 12),
+(2, 'B', 2),
+(3, 'Bob', 10),  -- Corrected score
+(4, 'D', 89),
+(5, 'E', 0),
+(6, 'F', 23),
+(7, 'G', 5),
+(8, 'H', 6),
+(9, 'I', -9),
+(10, 'J', 3)
+ON DUPLICATE KEY UPDATE
+    name = VALUES(name),
+    score = VALUES(score);
