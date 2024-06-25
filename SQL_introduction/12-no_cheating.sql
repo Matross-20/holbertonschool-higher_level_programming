d1bda2824bf7059cefb2a80808198a3d67b3466a
Matross-20 <matrossmthawelanga@gmail.com>
2024-06-25 18:41:04 +0000

    Cheating is bad

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
---a script that updates the score of Bob to 10 in the table second_table.
+-- a script that updates the score of Bob to 10 in the table second_table.
 
 UPDATE `second_table`
 SET score = 10
