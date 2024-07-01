99592a79a65a075d97b4ba3c4a9f2b8c161a63c0
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-07-01 17:02:06 +0200

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,46 +1,36 @@
 #!/usr/bin/python3
-"""
-Script to list all states with a name starting with
-'N' (upper N) from the database hbtn_0e_0_usa.
-"""
-
-import MySQLdb
 import sys
-
-
-def filter_states(username, password, database):
-    """
-    Connects to the MySQL server and lists all states
-    with a name starting with 'N'.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): Database name.
-    """
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
-        cursor = db.cursor()
-        cursor.execute("""SELECT * FROM states WHERE name
-                       LIKE BINARY 'N%' ORDER BY states.id""")
-        states = cursor.fetchall()
-        for state in states:
-            print(state)
-        cursor.close()
-        db.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-
+import MySQLdb
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        filter_states(sys.argv[1], sys.argv[2], sys.argv[3])
-    else:
-        print("Usage: ./1-filter_states.py <username> <password> <database>")
+    # Get the arguments from the command line
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
 
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=db_name
+    )
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Execute the query to get all states sorted by id
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(query)
+
+    # Fetch all the rows from the executed query
+    rows = cursor.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and the database connection
+    cursor.close()
+    db.close()
