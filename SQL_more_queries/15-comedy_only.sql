1b59890256e6af07f50d4cbebb791e5e737d9abf
Matross-20 <matrossmthawelanga@gmail.com>
2024-07-01 08:51:59 +0000

    Only Comedy

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- Script lists all Comedy shows in the database hbtn_0d_tvshows
+SELECT tv_shows.title
+FROM tv_shows, tv_show_genres, tv_genres
+WHERE tv_shows.id = tv_show_genres.show_id
+  AND tv_show_genres.genre_id = tv_genres.id
+  AND tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title ASC;
