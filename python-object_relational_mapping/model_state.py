ed1a6b00747a274325f68c6893c87ebbc39aaa3d
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-07-01 19:27:11 +0200

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,6 +8,8 @@ from sqlalchemy import (create_engine)
 
 from sqlalchemy.ext.declarative import declarative_base
 
+from sqlalchemy import Column, Integer, String
+
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
