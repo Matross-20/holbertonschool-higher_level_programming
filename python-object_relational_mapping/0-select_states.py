ff2615017a5d5dc35f9650a1eecc1295fa244fff
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-07-01 13:06:15 +0200

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,43 +1,21 @@
 #!/usr/bin/python3
-"""
-Script to list all states from the database hbtn_0e_0_usa.
-"""
-
-import MySQLdb
+# List all states from a given db sorted in ascending order by id
+# Username, password, and database names are given as user args
 import sys
-
-
-def list_states(username, password, database):
-    """
-    Connects to the MySQL server and lists all states from
-    the specified database.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): Database name.
-    """
-    try:
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        states = cursor.fetchall()
-        for state in states:
-            print(state)
-        cursor.close()
-        db.close()
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
-
+import MySQLdb
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        list_states(sys.argv[1], sys.argv[2], sys.argv[3])
-    else:
-        print("Usage: ./0-select_states.py <username> <password> <database>")
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+    allStates = cur.fetchall()
+
+    for state in allStates:
+        print(state)
+
+    cur.close()
+    db.close()
