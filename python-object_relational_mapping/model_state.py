64d3c9acab327abf674179e86d2cb19e753e52c5
Mthawelanga Matross <matrossmthawelanga@gmail.com>
2024-07-01 19:02:05 +0200

    First state model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -16,4 +16,4 @@ class State(Base):
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False) 
+    name = Column(String(128), nullable=False)  
