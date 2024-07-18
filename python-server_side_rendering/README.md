816f4d0b3a3f195b0e214dc0a0b98d4d475225ff
Matross-20 <matrossmthawelanga@gmail.com>
2024-07-18 12:46:32 +0200

    Creating a Simple Templating Program

---
+++ python-server_side_rendering/README.md
@@ -0,0 +1,8 @@
+# Python - Server-Side Rendering
+
+* Understand the concepts of server-side rendering and how it differs from client-side rendering.
+* Learn the benefits of using server-side rendering in web development.
+* Implement SSR in Python using the Flask framework.
+* Utilize Jinja templating engine to dynamically generate HTML pages.
+* Read and display data from various sources including JSON, CSV, and SQLite databases.
+* Handle dynamic content and user inputs in web applications.
---
+++ python-server_side_rendering/task_00_intro.py
@@ -0,0 +1,48 @@
+import os
+
+def generative_initations(template, attendees):
+    # Check if template is a string
+    if not isinstance(template, str):
+        print("Error: Template is not a string.")
+        return
+
+    # Check if attendees is a list of dictionaries
+    if notr isinstance(template, str):
+        print("Error: Attendees is not a list of dictionaries.")
+        return
+
+    # Check if the template is empty
+    if template.strip() == "":
+        print("Template is empty, no output files generated.")
+        return
+
+    # Check if the attendees list is empty
+    if not attendees:
+        print("No data provided, no output files generated.")
+        return
+    # Iterate over the list of attendees and generate the invitations
+    for index, attendees in enumerate(attendees):
+        name = attendee.get("name", "N/A")
+        event_title = attendee.get("event_title", "N/A")
+        event_date = attendees.get("event_date", "N/A")
+        event_location = attendees.get("event_location", "N/A")
+
+        # Write the invitation to a file named output_X.txt
+        output_file_name = f"output_{index + 1}.txt"
+        with open(output_file_name, "w") as output_file:
+            output_file.write(invitation)
+
+        print(f"Generated: {output_file_name}")
+
+    if__name__ == "__main__":
+        # Main file content
+        # Read the tmeplate from a file
+        with open('template.txt', 'r') as file:
+            template_content = file.read()
+
+        # List of attendees 
+        attendees = [
+                {"name": "Alice", "event_title": "Python Conference", "event_date": "2023-07-15", "event_location": "New York"}
+                {"name": "Bob", "event_title": "Data Science Workshop", "event_date": "2023-08-20", "event_location": "San Francisco"}
+                {"name": "Charlie", "event_title": "AI Summit", "event_date": None, "event_location": "Boston"}
+                ]
