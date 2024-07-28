92624e1fedc338218de3bbb8a7818f8756e76ccf
Matross-20 <matrossmthawelanga@gmail.com>
2024-07-28 15:19:10 +0200

    Javascript Manipulation

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,15 @@
+const myDiv = document.body.querySelector("#character");
+
+const fetched = fetch('https://swapi-api.hbtn.io/api/people/5/?format=json').then(
+    response => {
+        return response.json();
+    }
+).then(data => {
+    const name = data.name;
+    myDiv.textContent = name;
+})
+.catch(error => {
+    console.error('Error:', error);
+    myDiv.textContent = 'Error fetching character';
+});
+
