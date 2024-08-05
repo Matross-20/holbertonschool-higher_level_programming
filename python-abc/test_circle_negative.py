784a08f1571eda58fc0aaaadd32f70d065ffc188
Matross-20 <matrossmthawelanga@gmail.com>
2024-08-05 10:17:34 +0200

    Shapes, Interfaces, and Duck Typing

---
+++ python-abc/test_circle_negative.py
@@ -0,0 +1,12 @@
+import unittest
+from task_01_duck_typing import Circle
+
+class TestCircle(unittest.TestCase):
+    def test_circle_negative(self):
+        with self.assertRaises(ValueError) as context:
+            circle_negative = Circle(radius=-5)
+        self.assertEqual(str(context.exception), "Radius cannot be negative")
+
+if __name__ == "__main__":
+    unittest.main()
+
